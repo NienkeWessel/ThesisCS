75fe108e61eaa55d3c6239d7f1b926e8c6984d29
Nienke <n.wessel@xs4all.nl>
2023-10-23 17:41:11 +0200

    Allow for easy creation of multiple data splits from the same read in list

--- readdata.py
+++ readdata.py
@@ -35,7 +35,7 @@ def make_languages_string(languages, lang_split):
 
 
 def load_data(pw_filename, languages, lang_files, lang_split, comp_nr_lines, nr_lines, save=False, mode='most_common',
-              train_split=0.7, test_val_ratio=0.5):
+              train_split=0.7, test_val_ratio=0.5, nr_of_sets=3):
     passwords, comparison_pw = read_passwords(pw_filename, comp_nr_lines, nr_lines)
     words = []
     for language in languages:
@@ -48,44 +48,53 @@ def load_data(pw_filename, languages, lang_files, lang_split, comp_nr_lines, nr_
     all_words = passwords + words
     labels = (np.concatenate((np.ones(len(passwords)), np.zeros(len(words))), axis=0)).tolist()
 
-    train_words, test_val_words, train_labels, test_val_labels = train_test_split(all_words, labels,
-                                                                                  train_size=train_split,
-                                                                                  random_state=42)
+    random_state = 42
 
-    test_words, val_words, test_labels, val_labels = train_test_split(test_val_words, test_val_labels,
-                                                                      train_size=test_val_ratio,
-                                                                      random_state=42)
+    for n in range(nr_of_sets):
 
-    print("Training set size: {}\nTest set size: {}\nValidation set size: {}".format(len(train_words),
-                                                                                     len(test_words), len(val_words)))
+        train_words, test_val_words, train_labels, test_val_labels = train_test_split(all_words, labels,
+                                                                                      train_size=train_split,
+                                                                                      random_state=random_state)
 
-    if save:
-        data = DatasetDict({
-            'train': Dataset.from_dict({
-                'text': train_words,
-                'label': train_labels
-            }),
-            'validation': Dataset.from_dict({
-                'text': val_words,
-                'label': val_labels
-            }),
-            'test': Dataset.from_dict({
-                'text': test_words,
-                'label': test_labels
-            }),
-            'comparison_pw' : Dataset.from_dict({'text': comparison_pw}),
-        })
-        '''
-       'text': passwords + words,
-       'labels': (np.concatenate((np.ones(len(passwords)), np.zeros(len(words))), axis=0)).tolist(),
-       'comp_pw': comparison_pw,
-       '''
+        test_words, val_words, test_labels, val_labels = train_test_split(test_val_words, test_val_labels,
+                                                                          train_size=test_val_ratio,
+                                                                          random_state=random_state)
 
-        save_filename = mode + '_' + make_languages_string(languages, lang_split) + '_' + str(nr_lines)
-        #with open(save_filename, 'w') as f:
-        #    json.dump(data, f)
-        print(data)
-        data.save_to_disk(save_filename)
+        print("Training set size: {}\nTest set size: {}\nValidation set size: {}".format(len(train_words),
+                                                                                         len(test_words),
+                                                                                         len(val_words)))
+
+        if save:
+            data = DatasetDict({
+                'train': Dataset.from_dict({
+                    'text': train_words,
+                    'label': train_labels
+                }),
+                'validation': Dataset.from_dict({
+                    'text': val_words,
+                    'label': val_labels
+                }),
+                'test': Dataset.from_dict({
+                    'text': test_words,
+                    'label': test_labels
+                }),
+                'comparison_pw': Dataset.from_dict({
+                    'text': comparison_pw,
+                    'label': np.ones(len(comparison_pw))
+                }),
+            })
+            '''
+           'text': passwords + words,
+           'labels': (np.concatenate((np.ones(len(passwords)), np.zeros(len(words))), axis=0)).tolist(),
+           'comp_pw': comparison_pw,
+           '''
+
+            save_filename = mode + '_' + make_languages_string(languages, lang_split) + '_' + str(
+                nr_lines) + "_split" + str(n)
+            # with open(save_filename, 'w') as f:
+            #    json.dump(data, f)
+            print(data)
+            data.save_to_disk(save_filename)
     return passwords, words, comparison_pw
 
 
@@ -94,7 +103,7 @@ def load_data_from_file(filename):
         return json.load(f)
 
 
-'''
+
 languages = ['English', 'Spanish', 'Dutch', 'Arabic', 'Chinese']
 lang_files = {
     'English': "eng_news_2020_1M-words.txt",
@@ -113,4 +122,3 @@ load_data(pw_filename, languages, lang_files, lang_split, comp_nr_lines, nr_line
 s = make_languages_string(languages, lang_split)
 print(s)
 
-'''
