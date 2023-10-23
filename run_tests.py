aa94d4d6f37c7c9bffd2ac0d66e12d54fbeee778
Nienke <n.wessel@xs4all.nl>
2023-10-23 17:40:34 +0200

    Add functionalities to testing procedure: - allow for dummy dataset - add comments - add transformation helper function to prevent copy pasting - add printer function for data

--- run_tests.py
+++ run_tests.py
@@ -1,25 +1,60 @@
 import os
+import numpy as np
 from MLModel import MLModel
 #from readdata import load_data_from_file
-from datasets import DatasetDict, load_from_disk
+from datasets import DatasetDict, load_from_disk, Dataset
 
-from DataTransformer import FeatureDataTransformer, PytorchDataTransformer
+from DataTransformer import FeatureDataTransformer, PytorchDataTransformer, PassGPT10Transformer
 from FeatureModel import FeatureModel
 from PytorchModel import PytorchModel
+from PassGPTModel import GPTModel
+
 
 def find_files(dir):
+    """
+    Lists all files in a directory
+    :param dir: the directory of which the files need to be listed
+    :return: list of files in that directory
+    """
     return os.listdir(dir)
 
 
-def run_test_for_model(model, test_file_name):
+def create_dummy_dataset():
+    """
+    Creates a dummy dataset of the right format, so that you can test some toy data
+    :return:
+    """
+    dataset = Dataset.from_dict({
+        'text': ['password', 'normal'],
+        'label': [1.0, 0.0]
+    })
+    return dataset
+
+
+def transform_data(model, dataset, comparison_pw, internet = False):
+    """
+    Transforms the data by using a DataTransformer that fits the model type
+    :param model: the model the dataset is for
+    :param dataset: the dataset with at least a train, test and validation part
+    :param split: the part of the dataset you want; train, test or validation
+    :param internet: whether you have internet access
+    :return: the transformed data in a DataTransformer object
+    """
+    if isinstance(model, FeatureModel): # Check if we are dealing with a feature model
+        return FeatureDataTransformer(dataset, comparison_pw['text'])
+    elif isinstance(model, PytorchModel):
+        return PytorchDataTransformer(dataset)
+    elif isinstance(model, GPTModel):
+        return PassGPT10Transformer(dataset, internet=internet)
+    else:
+        return None
+
+
+def run_test_for_model(model, test_file_name, internet=False):
     dataset = load_from_disk(test_file_name)
 
-    if isinstance(model, FeatureModel): # Check if we are dealing with a feature model
-        train_data = FeatureDataTransformer(dataset['train'], dataset['comparison_pw']['text'])
-        test_data = FeatureDataTransformer(dataset['test'], dataset['comparison_pw']['text'])
-    elif isinstance(model, PytorchModel):
-        train_data = PytorchDataTransformer(dataset['train'])
-        test_data = PytorchDataTransformer(dataset['test'])
+    train_data = transform_data(model, dataset['train'], dataset['comparison_pw'], internet=internet)
+    test_data = transform_data(model, dataset['test'], dataset['comparison_pw'], internet=internet)
 
     model.train(train_data.X, train_data.y)
 
@@ -29,6 +64,25 @@ def run_test_for_model(model, test_file_name):
     print(accuracy)
 
 
+
+    dummy_dataset = create_dummy_dataset()
+    dummy_data = transform_data(model, dummy_dataset, dataset['comparison_pw'])
+    predictions = model.predict(dummy_data.X)
+    print(predictions)
+    accuracy = model.calc_accuracy(dummy_data.y, predictions)
+    print(accuracy)
+
+
+def print_dataset(dataset, split='train'):
+    """
+    Prints the whole dataset for a certain split of the dataset
+    :param dataset: the dataset that need to be printed
+    :param split: the split that needs to be printed (train, test, validation)
+    :return: None
+    """
+    print(dataset[split]['text'])
+    print(dataset[split]['label'])
+
 print(find_files('datasets'))
 
 #print(load_data_from_file('./testset_files/most_common_En1.0_1000'))
@@ -37,9 +91,10 @@ print(find_files('datasets'))
 
 #run_test_for_model(model, test_file_name)
 
-dataset = load_from_disk('./datasets/most_common_En1.0_1000')
+dataset = load_from_disk('./datasets/most_common_En1.0_50')
 print(dataset)
 
+print_dataset(dataset)
 
 
 from FeatureModel import RandomForest
@@ -47,4 +102,9 @@ model = RandomForest()
 
 #from PytorchModel import LSTMModel
 #model = LSTMModel()
-run_test_for_model(model, './datasets/most_common_En1.0_1000')
+
+internet = False
+#from PassGPTModel import PassGPT10Model
+#model = PassGPT10Model(internet=internet)
+
+run_test_for_model(model, './datasets/most_common_En1.0_1000_split1', internet=internet)
