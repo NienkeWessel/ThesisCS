a48de21b3843f8fae1c73abde4ffdfbb445585ed
Nienke <n.wessel@xs4all.nl>
2024-01-21 16:26:16 +0100

    Change way metrics are calculated for LSTM

--- PytorchModel.py
+++ PytorchModel.py
@@ -4,7 +4,6 @@ from LSTM import BiRNN
 from build_datasets import CharacterDataset
 
 import torch
-from torchmetrics.classification import BinaryRecall, BinaryAccuracy, BinaryPrecision, BinaryF1Score
 from d2l import torch as d2l
 import matplotlib.pyplot as plt
 
@@ -39,6 +38,8 @@ class PytorchModel(MLModel):
         train_loader = torch.utils.data.DataLoader(test, batch_size=self.batch_size, shuffle=False, drop_last=True)
         self.model.train(False)
 
+        self.model = self.model.to(device)
+
         results = None
         for i, x in enumerate(train_loader):
             x = x.to(device)
@@ -59,6 +60,33 @@ class PytorchModel(MLModel):
             torch.vstack(((pred[:, 0] > pred[:, 1]).unsqueeze(0), (pred[:, 0] <= pred[:, 1]).unsqueeze(0))), 0, 1)
         return (y_hat >= 0.5).to(y.dtype)
 
+    def confusion(self, pred, y):
+        """ Returns the confusion matrix for the values in the `prediction` and `truth`
+        tensors, i.e. the amount of positions where the values of `prediction`
+        and `truth` are
+        - 1 and 1 (True Positive)
+        - 1 and 0 (False Positive)
+        - 0 and 0 (True Negative)
+        - 0 and 1 (False Negative)
+
+        Code from https://gist.github.com/the-bass/cae9f3976866776dea17a5049013258d
+        """
+        confusion_vector = pred / y
+        # Element-wise division of the 2 tensors returns a new tensor which holds a
+        # unique value for each case:
+        #   1    where prediction and truth are 1 (True Positive)
+        #   inf  where prediction is 1 and truth is 0 (False Positive)
+        #   nan  where prediction and truth are 0(True Negative)
+        #   0    where prediction is 0 and truth is 1 (False Negative)
+
+        true_positives = torch.sum(confusion_vector == 1).item()
+        false_positives = torch.sum(confusion_vector == float('inf')).item()
+        true_negatives = torch.sum(torch.isnan(confusion_vector)).item()
+        false_negatives = torch.sum(confusion_vector == 0).item()
+
+        return true_positives, false_positives, true_negatives, false_negatives
+
+
     def calc_accuracy(self, y, pred):
 
         y = y.to(device)
@@ -72,11 +100,7 @@ class PytorchModel(MLModel):
         y_hat = (y_hat >= 0.5).to(y.dtype)
 
         correct = (y_hat == y).to(torch.float32)
-        # return torch.mean(correct)
-
-        pred = self.transform_pred(pred, y)
-        accuracy = BinaryAccuracy().to(device)
-        return accuracy(pred, y).tolist()  # torch.mean(correct).numpy()
+        return torch.mean(correct).tolist()
 
     def calc_recall(self, y, pred):
         y = y.to(device)
@@ -84,8 +108,8 @@ class PytorchModel(MLModel):
         if len(y) != len(pred):
             y = self.cut_of_y_to_batchsize(y)
         pred = self.transform_pred(pred, y)
-        recall = BinaryRecall().to(device)
-        return recall(pred, y).tolist()
+        tp, fp, tn, fn = self.confusion(pred, y)
+        return tp / (tp + fn)
 
     def calc_precision(self, y, pred):
         y = y.to(device)
@@ -93,8 +117,8 @@ class PytorchModel(MLModel):
         if len(y) != len(pred):
             y = self.cut_of_y_to_batchsize(y)
         pred = self.transform_pred(pred, y)
-        precision = BinaryPrecision().to(device)
-        return precision(pred, y).tolist()
+        tp, fp, tn, fn = self.confusion(pred, y)
+        return tp / (tp+fp)
 
     def calc_f1score(self, y, pred):
         y = y.to(device)
@@ -102,8 +126,10 @@ class PytorchModel(MLModel):
         if len(y) != len(pred):
             y = self.cut_of_y_to_batchsize(y)
         pred = self.transform_pred(pred, y)
-        f1score = BinaryF1Score().to(device)
-        return f1score(pred, y).tolist()
+        tp, fp, tn, fn = self.confusion(pred, y)
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)
+        return (2 * precision * recall) / (precision + recall)
 
     def load_model(self, filename):
         self.model.load_state_dict(torch.load(filename, map_location=torch.device(device)))
