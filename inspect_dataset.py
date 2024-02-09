44ad1204c72a59d637178775bbca8ca695845286
Nienke <n.wessel@xs4all.nl>
2024-02-09 22:43:01 +0100

    Function to filter out passwords of minimum length in test set

--- inspect_dataset.py
+++ inspect_dataset.py
@@ -10,4 +10,11 @@ def print_dataset(dataset, split='train'):
     print(dataset[split]['text'])
     print(dataset[split]['label'])
 
+def filter_data(dataset, split='test', min_length=16):
+    #return dataset
+    split = dataset[split]
+    return split.filter(lambda x: x['label'] == 1.0 and len(x['text'])>=min_length)
+
 print_dataset(load_from_disk('./most_common_Ar1.0_1000_split0'))
+print(load_from_disk('./long_passwords')['text'][:30])
+#print(filter_data(load_from_disk('./most_common_Ar1.0_500000_split0'))['text'])
